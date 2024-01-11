cc8e33581456c80df0cdaac41e305248b016f41e
Farukh <farukhk166@gmail.com>
2024-01-12 01:47:55 +0530

    initial commit

--- roadsiderunner/settings.py
+++ roadsiderunner/settings.py
@@ -78,17 +78,23 @@ WSGI_APPLICATION = 'roadsiderunner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME' : 'roadsiderunner',
+#        'USER' : 'root',
+#        'PASSWORD' : '',
+#        'HOST' : 'localhost',
+#        'PORT': '3306',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'roadsiderunner',
-        'USER' : 'root',
-        'PASSWORD' : '',
-        'HOST' : 'localhost',
-        'PORT': '3306',
-    }
+	"ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
